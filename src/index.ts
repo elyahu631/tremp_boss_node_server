ac328f4b28200643a081fb50e10d09c2c86c4cca
elyahu631 <eanavi363@gmail.com>
2023-06-29 18:08:29 +0300

    6

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-// src/index.ts
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 import { PORT } from './config/environment';
 import userRoutes from './resources/users/UserRoutes';  
 import adminRouter from './resources/adminUsers/AdminRoutes';  
+
 const app = express();
+app.use(cors());
 app.use(express.json());
+
 app.get('/', async (req: Request, res: Response) => {
   res.send('server runinig');
 });
