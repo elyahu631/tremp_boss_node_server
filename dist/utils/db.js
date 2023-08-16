952d1eec976a8a375491673de770da707b0348aa
ElyahuAnavi <84002556+elyahu631@users.noreply.github.com>
2023-08-16 22:18:02 +0300

    updat db erverless architecture

--- dist/utils/db.js
+++ dist/utils/db.js
@@ -14,97 +14,80 @@ const mongodb_1 = require("mongodb");
 const environment_1 = require("../config/environment");
 class DB {
     constructor() {
+        this.isConnected = false;
         this.client = new mongodb_1.MongoClient(environment_1.DB_URI);
         this.dbName = environment_1.DB_NAME;
         this.connect();
     }
     connect() {
         return __awaiter(this, void 0, void 0, function* () {
-            try {
-                yield this.client.connect();
-            }
-            catch (error) {
-                console.error(error);
-                process.exit(1); // terminate the process if the connection fails
+            if (!this.isConnected) {
+                try {
+                    yield this.client.connect();
+                    this.isConnected = true;
+                }
+                catch (error) {
+                    console.error(error);
+                    process.exit(1); // terminate the process if the connection fails
+                }
             }
         });
     }
     FindAll(collection, query = {}, projection = {}, sort = {}) {
         return __awaiter(this, void 0, void 0, function* () {
             try {
-                yield this.client.connect();
                 return yield this.client.db(this.dbName).collection(collection).find(query, { projection }).sort(sort).toArray();
                 ;
             }
             catch (error) {
                 throw error;
             }
-            finally {
-                yield this.client.close();
-            }
         });
     }
     FindByID(collection, id) {
         return __awaiter(this, void 0, void 0, function* () {
             try {
-                yield this.client.connect();
                 return yield this.client.db(this.dbName).collection(collection).findOne({ _id: new mongodb_1.ObjectId(id) });
             }
             catch (error) {
                 throw error;
-            }
-            finally {
-                yield this.client.close();
             }
         });
     }
     DeleteById(collection, id) {
         return __awaiter(this, void 0, void 0, function* () {
             try {
-                yield this.client.connect();
                 return yield this.client.db(this.dbName).collection(collection).deleteOne({ _id: new mongodb_1.ObjectId(id) });
             }
             catch (error) {
                 throw error;
-            }
-            finally {
-                yield this.client.close();
             }
         });
     }
     Insert(collection, doc) {
         return __awaiter(this, void 0, void 0, function* () {
             try {
-                yield this.client.connect();
                 return yield this.client.db(this.dbName).collection(collection).insertOne(doc);
             }
             catch (error) {
                 return error;
-            }
-            finally {
-                yield this.client.close();
             }
         });
     }
     Update(collection, id, updatedDocument) {
         return __awaiter(this, void 0, void 0, function* () {
             try {
-                yield this.client.connect();
                 const result = yield this.client.db(this.dbName).collection(collection).updateOne({ _id: new mongodb_1.ObjectId(id) }, { $set: updatedDocument });
                 return result;
             }
             catch (error) {
                 throw error;
             }
-            finally {
-                yield this.client.close();
-            }
         });
     }
     UpdateWithOperation(collection, id, updateOperation) {
         return __awaiter(this, void 0, void 0, function* () {
             try {
-                yield this.client.connect();
                 const result = yield this.client.db(this.dbName).collection(collection).updateOne({ _id: new mongodb_1.ObjectId(id) }, updateOperation);
                 console.log(result);
                 return result;
@@ -112,22 +95,15 @@ class DB {
             catch (error) {
                 throw error;
             }
-            finally {
-                yield this.client.close();
-            }
         });
     }
     aggregate(collection, pipeline = [{}]) {
         return __awaiter(this, void 0, void 0, function* () {
             try {
-                yield this.client.connect();
                 return yield this.client.db(this.dbName).collection(collection).aggregate(pipeline).toArray();
             }
             catch (error) {
                 throw error;
-            }
-            finally {
-                yield this.client.close();
             }
         });
     }
--- src/utils/db.ts
+++ src/utils/db.ts
@@ -5,6 +5,7 @@ import { Model } from '../config/models'
 class DB {
   client: MongoClient;
   dbName: string;
+  isConnected: boolean = false;
 
   constructor() {
     this.client = new MongoClient(DB_URI);
@@ -13,73 +14,60 @@ class DB {
   }
 
   async connect() {
-    try {
-      await this.client.connect();
-    } catch (error) {
-      console.error(error);
-      process.exit(1); // terminate the process if the connection fails
+    if (!this.isConnected) {
+      try {
+        await this.client.connect();
+        this.isConnected = true;
+      } catch (error) {
+        console.error(error);
+        process.exit(1); // terminate the process if the connection fails
+      }
     }
   }
 
   async FindAll(collection: string, query = {}, projection = {}, sort = {}) {
     try {
-      await this.client.connect();
       return await this.client.db(this.dbName).collection(collection).find(query, { projection }).sort(sort).toArray();;
     } catch (error) {
       throw error
-    } finally {
-      await this.client.close();
-    }
+    } 
   }
 
   async FindByID(collection: string, id: string) {
     try {
-      await this.client.connect();
       return await this.client.db(this.dbName).collection(collection).findOne({ _id: new ObjectId(id) });
     } catch (error) {
       throw error;
-    } finally {
-      await this.client.close();
-    }
+    } 
   }
 
   async DeleteById(collection: string, id: string) {
     try {
-      await this.client.connect();
       return await this.client.db(this.dbName).collection(collection).deleteOne({ _id: new ObjectId(id) });
     } catch (error) {
       throw error;
-    } finally {
-      await this.client.close();
-    }
+    } 
   }
 
   async Insert(collection: string, doc: Model) {
     try {
-      await this.client.connect();
       return await this.client.db(this.dbName).collection(collection).insertOne(doc);
     } catch (error) {
       return error;
-    } finally {
-      await this.client.close();
-    }
+    } 
   }
 
   async Update(collection: string, id: string, updatedDocument: Partial<Model>) {
     try {
-      await this.client.connect();
       const result = await this.client.db(this.dbName).collection(collection).updateOne({ _id: new ObjectId(id) }, { $set: updatedDocument });
       return result;
     } catch (error) {
       throw error;
-    } finally {
-      await this.client.close();
-    }
+    } 
   }
 
   async UpdateWithOperation(collection: string, id: string, updateOperation: object) {
     try {
-      await this.client.connect();
       const result = await this.client.db(this.dbName).collection(collection).updateOne(
         { _id: new ObjectId(id) },
         updateOperation
@@ -88,20 +76,15 @@ class DB {
       return result;
     } catch (error) {
       throw error;
-    } finally {
-      await this.client.close();
-    }
+    } 
   }
 
   async aggregate(collection: string, pipeline = [{}]) {
     try {
-      await this.client.connect();
       return await this.client.db(this.dbName).collection(collection).aggregate(pipeline).toArray();
     } catch (error) {
       throw error;
-    } finally {
-      await this.client.close();
-    }
+    } 
   }
 
 }
